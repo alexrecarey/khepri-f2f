257718a9ce59e1495b1b23d65517d4101d7eb418
Alex Recarey <alex@recarey.org>
2023-02-02 13:11:47 +0100

    Getting the form up and running

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,71 @@
 import { useState } from 'react'
 import CssBaseline from '@mui/material/CssBaseline';
 import './App.css'
-import {BottomNavigation, BottomNavigationAction} from "@mui/material";
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import RestoreIcon from '@mui/icons-material/Restore';
+import {
+  Grid,
+  Rating,
+  TextField,
+  Typography
+} from "@mui/material";
 
 function App() {
-  const [tab, setTab] = useState(0)
+  const [burstA, setBurstA] = useState(3)
+  const [burstB, setBurstB] = useState(1)
+  const [successValueA, setSuccessValueA] = useState(10)
+  const [successValueB, setSuccessValueB] = useState(10)
+
 
   return (
     <>
-      <CssBaseline />
-      <BottomNavigation
-        showLabels
-        value={tab}
-        onChange={(event, newValue) => {
-          setTab(newValue);
-        }}
-      >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-      </BottomNavigation>
+    <CssBaseline />
+        <Grid container spacing={2}>
+          <Typography>Player A</Typography>
+          <Grid xs={12} item>
+            <Rating
+              value={burstA}
+              min={1}
+              max={6}
+              onChange={(event, newValue) => {
+                setBurstA(newValue);
+              }
+          }
+            />
+          </Grid>
+          <Grid xs={12} item>Burst</Grid>
+
+          <Grid xs={12} item>Success Value</Grid>
+          <TextField
+            inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+            value={successValueA}
+            onChange={(event, newValue)=> {
+              setSuccessValueA(newValue);
+            }}
+          />
+
+          <Grid xs={12} item>Player B</Grid>
+          <Grid xs={12} item>Burst</Grid>
+          <Grid xs={12} item>
+            <Rating
+              value={burstB}
+              min={1}
+              max={6}
+              onChange={(event, newValue) => {
+                setBurstB(newValue);
+              }}
+            />
+          </Grid>
+          <Grid xs={12} item>Success Value</Grid>
+          <Grid xs={12} item>
+            <TextField
+              inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+              value={successValueB}
+              onChange={(event, newValue)=> {
+                setSuccessValueB(newValue);
+              }}
+            />
+          </Grid>
+
+        </Grid>
     </>
   )
 }
