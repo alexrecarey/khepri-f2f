alexrecarey/khepri-f2f

commit 3dc57e0251234a2f03f0723a8ce8709432f816ef

Added ability to calculate fixed face to face rolls.
This allows us to correctly calculate mechanics like AC2 and
mega beasts, that are assumed to always roll a certain value
in the Face to Face roll.

Also changed syntax to of die rolls to the one suggested by
@HighDiceRoller and it's *much* clearer what the code is doing
now. Thank you!

src/python.worker.js
@@ -69,13 +69,27 @@ def dtw_vs_dodge(dtw_burst, dodge_sv, dodge_burst):
     if dodge_burst == 0:
         result.append(((0, dtw_burst, 0, 0), 1))
     else:
-        dDodge = Die([(1 if x <= dodge_sv else 0) for x in range(20)])
+        dDodge = d20 <= dodge_sv
         for outcome, amount in (dodge_burst @ dDodge).items():
             if outcome >= 1:
                 result.append(((0, 0, 0, 0), amount))
             elif outcome == 0:
                 result.append(((0, dtw_burst, 0, 0), amount))
     return result
+
+
+def fixed_face_to_face(player_a_sv, player_a_burst, player_b_sv, player_b_burst):
+    # Handle cases where SV > 20
+    a_sv = player_a_sv if player_a_sv <= 20 else 20
+    a_bonus = 0 if player_a_sv <= 20 else player_a_sv - 20
+    b_sv = 21  # make success value 21 to avoid crits
+    b_die_face = player_b_sv if player_b_sv <= 20 else 20
+    b_die = Die([b_die_face])  # Create a special die that always rolls the same number
+
+    result = InfinityFace2FaceEvaluator(a_sv=a_sv, b_sv=b_sv).evaluate(
+        lowest(d20+a_bonus, 20).pool(player_a_burst),
+        b_die.pool(player_b_burst))
+    return [i for i in result.items()]
 
 
 def face_to_face_result(outcomes):
@@ -181,11 +195,11 @@ def face_to_face_expected_wounds(
 
         # Generate die with 1's for wounds and 0's for successful armor saves
         if cont:
-            dSave = Die([(damage + Again() if x < armor_save else 0) for x in range(20)], again_depth=5)
+            dSave = Die([(damage + Again() if x <= armor_save else 0) for x in range(1, 21)], again_depth=5)
         else:
-            dSave = Die([(damage if x < armor_save else 0) for x in range(20)])
-        dCrit = Die([(1 if x < armor_save else 0) for x in range(20)])
-        dPlasma = Die([(1 if x < bts_save else 0) for x in range(20)])
+            dSave = d20 <= armor_save
+        dCrit = d20 <= armor_save
+        dPlasma = d20 <= bts_save
 
         r = Pool(
             [dSave for x in range(saves)] + [dCrit for x in range(crit_saves)] + [dPlasma for x in range(plasma_saves)]
@@ -253,9 +267,11 @@ def format_expected_wounds(wounds, max_wounds_shown=3):
 def roll_and_bridge_results(
         player_a_sv, player_a_burst, player_a_dam, player_a_arm, player_a_bts, player_a_ammo, player_a_cont, player_a_crit_immune,
         player_b_sv, player_b_burst, player_b_dam, player_b_arm, player_b_bts, player_b_ammo, player_b_cont, player_b_crit_immune,
-        dtw):
+        dtw, fixed):
     if dtw:
         outcomes = dtw_vs_dodge(player_a_burst, player_b_sv, player_b_burst)  # dtw_burst, dodge_sv, dodge_burst
+    elif fixed:
+        outcomes = fixed_face_to_face(player_a_sv, player_a_burst, player_b_sv, player_b_burst)
     else:
         outcomes = face_to_face(player_a_sv, player_a_burst, player_b_sv, player_b_burst)
     results = face_to_face_result(outcomes)
@@ -297,7 +313,7 @@ async function calculateProbability(p) {
     p['contA'], p['critImmuneA'],
     p['successValueB'], p['burstB'], p['damageB'], p['armB'], p['btsB'], p['ammoB'],
     p['contB'], p['critImmuneB'],
-    p['dtwVsDodge'])
+    p['dtwVsDodge'], p['fixedFaceToFace'])
 }
 
 

src/python/f2f.py
@@ -1,7 +1,7 @@
-import micropip
-await micropip.install('icepool==0.20.1')
-import js
-from pyodide.ffi import to_js
+# import micropip
+# await micropip.install('icepool==0.20.1')
+# import js
+# from pyodide.ffi import to_js
 import icepool
 from icepool import d20, lowest, Again, Die, Pool
 from math import comb
@@ -67,13 +67,26 @@ def dtw_vs_dodge(dtw_burst, dodge_sv, dodge_burst):
     if dodge_burst == 0:
         result.append(((0, dtw_burst, 0, 0), 1))
     else:
-        dDodge = Die([(1 if x <= dodge_sv else 0) for x in range(20)])
+        dDodge = d20 <= dodge_sv
         for outcome, amount in (dodge_burst @ dDodge).items():
             if outcome >= 1:
                 result.append(((0, 0, 0, 0), amount))
             elif outcome == 0:
                 result.append(((0, dtw_burst, 0, 0), amount))
     return result
+
+
+def fixed_face_to_face(player_a_sv, player_a_burst, player_b_sv, player_b_burst):
+    # Handle cases where SV > 20
+    a_sv = player_a_sv if player_a_sv <= 20 else 20
+    a_bonus = 0 if player_a_sv <= 20 else player_a_sv - 20
+    b_sv = 21  # make success value 21 to avoid crits
+    b_die = Die([player_b_sv])  # Create a special die that always rolls the same number
+
+    result = InfinityFace2FaceEvaluator(a_sv=a_sv, b_sv=b_sv).evaluate(
+        lowest(d20+a_bonus, 20).pool(player_a_burst),
+        b_die.pool(player_b_burst))
+    return [i for i in result.items()]
 
 
 def face_to_face_result(outcomes):
@@ -179,22 +192,18 @@ def face_to_face_expected_wounds(
 
         # Generate die with 1's for wounds and 0's for successful armor saves
         if cont:
-            dSave = Die([(damage + Again() if x < armor_save else 0) for x in range(20)], again_depth=5)
+            dSave = Die([(damage + Again() if x <= armor_save else 0) for x in range(1, 21)], again_depth=5)
         else:
-            dSave = Die([(damage if x < armor_save else 0) for x in range(20)])
-        dCrit = Die([(1 if x < armor_save else 0) for x in range(20)])
-        dPlasma = Die([(1 if x < bts_save else 0) for x in range(20)])
+            dSave = d20 <= armor_save
+        dCrit = d20 <= armor_save
+        dPlasma = d20 <= bts_save
 
         r = Pool(
             [dSave for x in range(saves)] + [dCrit for x in range(crit_saves)] + [dPlasma for x in range(plasma_saves)]
         ).sum()
         denominator = r.denominator()
         for w, occurrences in r.items():
-            if w == 0:
-                wounds['fail'][0] = wounds['fail'].get(0, 0.0) + (occurrences/denominator) * rolls
-                wounds['guts'][counterpart] += (occurrences/denominator) * rolls
-            else:
-                wounds[winner][w] = wounds[winner].get(w, 0) + (occurrences/denominator) * rolls
+            wounds[winner][w] = wounds[winner].get(w, 0) + (occurrences/denominator) * rolls
     return wounds
 
 
@@ -210,13 +219,7 @@ def format_face_to_face(face_to_face):
     return output
 
 
-def format_expected_wounds(wounds, max_wounds_shown=3):
-    """Format expected_wounds into a list of results
-
-    Output format is {'player': 'active/reactive/fail', 'wounds': 3, 'chance': 0.2432, 'raw_chance' 1341234.23,
-                      'cumulative_chance': 0.53234, 'raw_guts_chance': 0, 'guts_chance': 0.0234, 'cumulative_guts': 0.0723}
-    """
-    # Squash items that are > than max_wounds_shown
+def consolidate_wounds_over_maximum(wounds, max_wounds_shown=3):
     squashed = {'active': None, 'reactive': None, 'fail': wounds['fail']}
     for player in ['active', 'reactive']:
         over_max = {k: v for k, v in wounds[player].items() if k > max_wounds_shown}
@@ -227,14 +230,18 @@ def format_expected_wounds(wounds, max_wounds_shown=3):
             squashed[player] = new_dict
         else:
             squashed[player] = wounds[player]
+    return squashed
 
-    # Create output table
-    active_base_guts = wounds['guts']['active']
-    reactive_base_guts = wounds['guts']['reactive']
-    counterpart = {
-        'active': 'reactive',
-        'reactive': 'active'
-    }
+
+def format_expected_wounds(wounds, max_wounds_shown=3):
+    """Format expected_wounds into a list of results
+
+    Output format is {'player': 'active/reactive/fail', 'wounds': 3, 'chance': 0.2432, 'raw_chance' 1341234.23,
+                      'cumulative_chance': 0.53234, 'raw_guts_chance': 0, 'guts_chance': 0.0234, 'cumulative_guts': 0.0723}
+    """
+    # Squash items that are > than max_wounds_shown
+    total_rolls = wounds['total_rolls']
+    squashed = consolidate_wounds_over_maximum(wounds, max_wounds_shown=max_wounds_shown)
     expected_wounds = []
     index = 0
     for player in ['active', 'fail', 'reactive']:
@@ -245,18 +252,10 @@ def format_expected_wounds(wounds, max_wounds_shown=3):
                 'player': player,
                 'wounds': key,
                 'raw_chance': squashed[player][key],
+                'chance': squashed[player][key]/total_rolls,
                 'cumulative_chance': reduce(
                     lambda x, y: x+y,
-                    [squashed[player][i] for i in squashed[player].keys() if i >= key], 0) / wounds['total_rolls'],
-                'chance': squashed[player][key]/wounds['total_rolls'],
-                'raw_active_guts': active_base_guts if player == 'fail' else squashed[counterpart[player]].get(key, 0),
-                'raw_reactive_guts': reactive_base_guts if player == 'fail' else squashed[counterpart[player]].get(key, 0),
-                'cumulative_active_guts_chance': 0 if player == 'active' else (
-                    reduce(lambda x, y: x+y, [squashed['reactive'][i] for i in squashed['reactive'].keys() if i <= key], 0)
-                    + active_base_guts) / wounds['total_rolls'],
-                'cumulative_reactive_guts_chance': 0 if player == 'reactive' else (
-                    reduce(lambda x, y: x+y, [squashed['active'][i] for i in squashed['active'].keys() if i <= key], 0)
-                    + reactive_base_guts) / wounds['total_rolls'],
+                    [squashed[player][i] for i in squashed[player].keys() if i >= key], 0) / total_rolls,
             })
             index += 1
     return expected_wounds
@@ -265,9 +264,11 @@ def format_expected_wounds(wounds, max_wounds_shown=3):
 def roll_and_bridge_results(
         player_a_sv, player_a_burst, player_a_dam, player_a_arm, player_a_bts, player_a_ammo, player_a_cont, player_a_crit_immune,
         player_b_sv, player_b_burst, player_b_dam, player_b_arm, player_b_bts, player_b_ammo, player_b_cont, player_b_crit_immune,
-        dtw):
+        dtw, fixed):
     if dtw:
         outcomes = dtw_vs_dodge(player_a_burst, player_b_sv, player_b_burst)  # dtw_burst, dodge_sv, dodge_burst
+    elif fixed:
+        outcomes = fixed_face_to_face(player_a_sv, player_a_burst, player_b_sv, player_b_burst)
     else:
         outcomes = face_to_face(player_a_sv, player_a_burst, player_b_sv, player_b_burst)
     results = face_to_face_result(outcomes)
@@ -283,8 +284,10 @@ def roll_and_bridge_results(
         'expected_wounds': formatted_expected_wounds,
         'total_rolls': expected_wounds['total_rolls']
     }
-    return to_js(return_object, dict_converter=js.Object.fromEntries)
-    # return return_object
+    # return to_js(return_object, dict_converter=js.Object.fromEntries)
+    return return_object
 
 # Return value for Javascript
-to_js(roll_and_bridge_results)
+#to_js(roll_and_bridge_results)
+
+

src/python/test_f2f.py
@@ -1,9 +1,12 @@
-from f2f import face_to_face_expected_wounds
+from functools import reduce
+
+import f2f
+from f2f import face_to_face_expected_wounds, face_to_face, consolidate_wounds_over_maximum
 import re
 from math import isclose
 
 
-def ghostlords_to_dict(string):
+def ghostlords_raw_to_dict(string):
     regex = r'(P\d) Scores\s+(\d\d*)\s*Success\(es\):\s*(\d*\.?\d+)%$'
     fail_regex = r'No Successes:\s+(\d*\.?\d+)%'
     go = {
@@ -24,12 +27,58 @@ def ghostlords_to_dict(string):
     return go
 
 
-def khepri_result_to_percentage(khepri_dict):
-    # Yeah, I could nest list comprehensions, but I'd like to be able to read this code
+def ghostlords_minimal_to_dict(active_str, reactive_str, fail_str):
+    """Example minimal result
+
+    Active Player
+
+29.33% Custom Unit inflicts 1 or more wounds on Custom Unit (1 W)
+10.11% Custom Unit inflicts 2 or more wounds on Custom Unit (Unconscious)
+3.08% Custom Unit inflicts 3 or more wounds on Custom Unit (Dead)
+
+Failures
+
+49.46% Neither player succeeds
+Reactive Player
+
+21.20% Custom Unit inflicts 1 or more wounds on Custom Unit (1 W)
+10.10% Custom Unit inflicts 2 or more wounds on Custom Unit (Unconscious)
+2.19% Custom Unit inflicts 3 or more wounds on Custom Unit (Dead)"""
+
+    regex = r'(\d*\.?\d+)% Custom Unit inflicts (\d) or more'
+    fail_regex = r'(\d*\.?\d+)% Neither player succeeds'
+    active = {}
+    reactive = {}
+    fail = {}
+
+    for m in re.finditer(regex, active_str, re.MULTILINE):
+        active[int(m[2])] = float(m[1]) / 100
+
+    for m in re.finditer(regex, reactive_str, re.MULTILINE):
+        reactive[int(m[2])] = float(m[1]) / 100
+
+    for m in re.findall(fail_regex, fail_str, re.MULTILINE):
+        fail[0] = float(m) / 100
+
+    # find the values not the cummulative ones.
     return {
-        'active':{k: v/khepri_dict['total_rolls'] for k, v in khepri_dict['active'].items()},
-        'reactive': {k: v/khepri_dict['total_rolls'] for k, v in khepri_dict['reactive'].items()},
-        'fail': {k: v/khepri_dict['total_rolls'] for k, v in khepri_dict['fail'].items()}}
+        'active': {k: (v - active.get(k+1, 0)) for k, v in active.items()},
+        'reactive': {k: (v - reactive.get(k+1, 0)) for k, v in reactive.items()},
+        'fail': {0: fail[0]}
+    }
+
+
+def khepri_result_to_percentage(khepri_dict, max_wounds_shown=100):
+    # Yeah, I could nest list comprehensions, but I'd like to be able to read this code
+    # As I've changed my return format, we now have to exclude 0 wounds from active and reactive successes
+    # we also have to add 0 wounds active and 0 wounds reactive to failure case
+    failure_rolls = khepri_dict['active'].get(0, 0) + khepri_dict['reactive'].get(0,0) + khepri_dict['fail'].get(0,0)
+    wounds = {
+        'active': {k: v/khepri_dict['total_rolls'] for k, v in khepri_dict['active'].items() if k > 0},
+        'reactive': {k: v/khepri_dict['total_rolls'] for k, v in khepri_dict['reactive'].items() if k > 0},
+        'fail': {0: failure_rolls/khepri_dict['total_rolls']}
+    }
+    return f2f.consolidate_wounds_over_maximum(wounds, max_wounds_shown=max_wounds_shown)
 
 
 def is_ghostlords_equal(gl_dict, kp_dict):
@@ -45,7 +94,6 @@ def is_ghostlords_equal(gl_dict, kp_dict):
                 print(f'For {player}[{outcome}] GL({gl_dict[player][outcome]}) not close to KP({kp_dict[player][outcome]})')
                 equals_flag = False
     return equals_flag
-
 
 
 class TestAgainstGhostlordsCalculator:
@@ -99,10 +147,12 @@ P2 Scores  1+ Successes:   8.987%"""
         player_b_dam_ = 14
         player_b_arm_ = 5
         player_b_ammo_ = 'N'
+        outcomes = face_to_face(player_a_sv_, player_a_burst_, player_b_sv_, player_b_burst_)
         kp_result = face_to_face_expected_wounds(
-            player_a_sv_, player_a_burst_, player_a_dam_, player_a_arm_, player_a_ammo_, False,
-            player_b_sv_, player_b_burst_, player_b_dam_, player_b_arm_, player_b_ammo_, False)
-        gl_dict = ghostlords_to_dict(gl_result)
+            outcomes,
+            player_a_dam_, player_a_arm_, player_a_ammo_,
+            player_b_dam_, player_b_arm_, player_b_ammo_)
+        gl_dict = ghostlords_raw_to_dict(gl_result)
         kp_dict = khepri_result_to_percentage(kp_result)
         is_ghostlords_equal(gl_dict, kp_dict)
         assert is_ghostlords_equal(gl_dict, kp_dict)
@@ -143,10 +193,12 @@ P2 Scores  1+ Successes:   4.654%"""
         player_b_dam_ = 12
         player_b_arm_ = 4
         player_b_ammo_ = 'N'
+        outcomes = face_to_face(player_a_sv_, player_a_burst_, player_b_sv_, player_b_burst_)
         kp_result = face_to_face_expected_wounds(
-            player_a_sv_, player_a_burst_, player_a_dam_, player_a_arm_, player_a_ammo_, False,
-            player_b_sv_, player_b_burst_, player_b_dam_, player_b_arm_, player_b_ammo_, False)
-        gl_dict = ghostlords_to_dict(gl_result)
+            outcomes,
+            player_a_dam_, player_a_arm_, player_a_ammo_,
+            player_b_dam_, player_b_arm_, player_b_ammo_)
+        gl_dict = ghostlords_raw_to_dict(gl_result)
         kp_dict = khepri_result_to_percentage(kp_result)
         is_ghostlords_equal(gl_dict, kp_dict)
         assert is_ghostlords_equal(gl_dict, kp_dict)
@@ -263,10 +315,12 @@ P2 Scores  1+ Successes:  14.245%"""
         player_b_dam_ = 12
         player_b_arm_ = 3
         player_b_ammo_ = 'N'
+        outcomes = face_to_face(player_a_sv_, player_a_burst_, player_b_sv_, player_b_burst_)
         kp_result = face_to_face_expected_wounds(
-            player_a_sv_, player_a_burst_, player_a_dam_, player_a_arm_, player_a_ammo_, False,
-            player_b_sv_, player_b_burst_, player_b_dam_, player_b_arm_, player_b_ammo_, False)
-        gl_dict = ghostlords_to_dict(gl_result)
+            outcomes,
+            player_a_dam_, player_a_arm_, player_a_ammo_,
+            player_b_dam_, player_b_arm_, player_b_ammo_)
+        gl_dict = ghostlords_raw_to_dict(gl_result)
         kp_dict = khepri_result_to_percentage(kp_result)
         assert is_ghostlords_equal(gl_dict, kp_dict)
 
@@ -358,10 +412,12 @@ P2 Scores  1+ Successes:  20.088%"""
         player_b_dam_ = 15
         player_b_arm_ = 3
         player_b_ammo_ = 'N'
+        outcomes = face_to_face(player_a_sv_, player_a_burst_, player_b_sv_, player_b_burst_)
         kp_result = face_to_face_expected_wounds(
-            player_a_sv_, player_a_burst_, player_a_dam_, player_a_arm_, player_a_ammo_, False,
-            player_b_sv_, player_b_burst_, player_b_dam_, player_b_arm_, player_b_ammo_, False)
-        gl_dict = ghostlords_to_dict(gl_result)
+            outcomes,
+            player_a_dam_, player_a_arm_, player_a_ammo_,
+            player_b_dam_, player_b_arm_, player_b_ammo_)
+        gl_dict = ghostlords_raw_to_dict(gl_result)
         kp_dict = khepri_result_to_percentage(kp_result)
         assert is_ghostlords_equal(gl_dict, kp_dict)
 
@@ -429,9 +485,98 @@ P2 Scores  1+ Successes:  22.705%"""
         player_b_dam_ = 16
         player_b_arm_ = 11
         player_b_ammo_ = 'EXP'
+        outcomes = face_to_face(player_a_sv_, player_a_burst_, player_b_sv_, player_b_burst_)
         kp_result = face_to_face_expected_wounds(
-            player_a_sv_, player_a_burst_, player_a_dam_, player_a_arm_, player_a_ammo_, False,
-            player_b_sv_, player_b_burst_, player_b_dam_, player_b_arm_, player_b_ammo_, False)
-        gl_dict = ghostlords_to_dict(gl_result)
+            outcomes,
+            player_a_dam_, player_a_arm_, player_a_ammo_,
+            player_b_dam_, player_b_arm_, player_b_ammo_)
+        gl_dict = ghostlords_raw_to_dict(gl_result)
         kp_dict = khepri_result_to_percentage(kp_result)
         assert is_ghostlords_equal(gl_dict, kp_dict)
+
+
+    def test_continuous_damage_azrail_vs_tag(self):
+        print("Testing Azra'il HMG vs TAG")
+        active_str = """29.33% Custom Unit inflicts 1 or more wounds on Custom Unit (1 W)
+10.11% Custom Unit inflicts 2 or more wounds on Custom Unit (Unconscious)
+3.08% Custom Unit inflicts 3 or more wounds on Custom Unit (Dead)"""
+        failure_str = """49.46% Neither player succeeds"""
+        reactive_str = """21.20% Custom Unit inflicts 1 or more wounds on Custom Unit (1 W)
+10.10% Custom Unit inflicts 2 or more wounds on Custom Unit (Unconscious)
+2.19% Custom Unit inflicts 3 or more wounds on Custom Unit (Dead)"""
+        gl_dict = {
+            'active': {1: 0.2933, 2: 0.1011, 3: 0.0308},
+            'reactive': {1: 0.2120, 2: 0.1010, 3: 0.0219},
+            'fail': {0: 0.4946}
+        }
+        player_a_sv_ = 13
+        player_a_burst_ = 3
+        player_a_dam_ = 14
+        player_a_arm_ = 8
+        player_a_ammo_ = 'DA'
+        player_b_sv_ = 14
+        player_b_burst_ = 1
+        player_b_dam_ = 16
+        player_b_arm_ = 11
+        player_b_ammo_ = 'EXP'
+        player_a_cont_ = True
+        player_b_cont_ = False
+        outcomes = face_to_face(player_a_sv_, player_a_burst_, player_b_sv_, player_b_burst_)
+        kp_result = face_to_face_expected_wounds(
+            outcomes,
+            player_a_dam_, player_a_arm_, player_a_ammo_,
+            player_b_dam_, player_b_arm_, player_b_ammo_, player_a_cont=player_a_cont_)
+        gl_dict = ghostlords_minimal_to_dict(active_str=active_str, reactive_str=reactive_str, fail_str=failure_str)
+        kp_dict = khepri_result_to_percentage(kp_result, max_wounds_shown=3)
+        assert is_ghostlords_equal(gl_dict, kp_dict)
+
+
+
+# TODO: Crit immune tests. Note: GL does not have a crit immune toggle
+# TODO: Dodge vs template weapon test
+# TODO: plasma tests
+# TODO: Extreme values tests: Reactive 0 burst
+# TODO: Extreme values tests: Minimum 5+ wounds
+# TODO: Extreme values tests: Active and reactive both cause 0 wounds
+
+
+# class SelfTests:
+    # def test_simple_continuous_damage(self):
+    #     player_a_sv_ = 20
+    #     player_a_burst_ = 1
+    #     player_a_dam_ = 10
+    #     player_a_arm_ = 0
+    #     player_a_ammo_ = 'N'
+    #     player_b_sv_ = 0
+    #     player_b_burst_ = 1
+    #     player_b_dam_ = 10
+    #     player_b_arm_ = 0
+    #     player_b_ammo_ = 'N'
+    #     result = face_to_face_expected_wounds(
+    #         player_a_sv_, player_a_burst_, player_a_dam_, player_a_arm_, player_a_ammo_,
+    #         player_b_sv_, player_b_burst_, player_b_dam_, player_b_arm_, player_b_ammo_, player_a_cont=True)
+        # Ghostlords calculator result
+        # P1 Scores 20+ Successes:   0.000%
+        # P1 Scores 19+ Successes:   0.000%
+        # P1 Scores 18+ Successes:   0.000%
+        # P1 Scores 17+ Successes:   0.001%
+        # P1 Scores 16+ Successes:   0.002%
+        # P1 Scores 15+ Successes:   0.003%
+        # P1 Scores 14+ Successes:   0.006%
+        # P1 Scores 13+ Successes:   0.013%
+        # P1 Scores 12+ Successes:   0.025%
+        # P1 Scores 11+ Successes:   0.050%
+        # P1 Scores 10+ Successes:   0.100%
+        # P1 Scores  9+ Successes:   0.200%
+        # P1 Scores  8+ Successes:   0.400%
+        # P1 Scores  7+ Successes:   0.801%
+        # P1 Scores  6+ Successes:   1.602%
+        # P1 Scores  5+ Successes:   3.203%
+        # P1 Scores  4+ Successes:   6.406%
+        # P1 Scores  3+ Successes:  12.812%
+        # P1 Scores  2+ Successes:  25.625%
+        # P1 Scores  1+ Successes:  51.250%
+    #     assert result == {'active': {1: 132982387311.80006, 2: 120001405942.39105, 3: 65727164981.19168, 4: 22266942310.045437,
+    #                                  5: 4287388973.3965435, 6: 358155514.2130559},
+    #                       'reactive': {1: 41592289828.72894, 2: 16712956943.140924, 3: 857520144.1465032, 4: 36956664.07491093,
+    #                                    5: 1074799.1400103127}, 'fail': {0: 107175756587.7309}, 'total_rolls': 512000000000}

commit faa3422bac7a3e27cd68018b4f8c1f791c29a092

BUGFIX: 3dc57e0251234a2f03f0723a8ce8709432f816ef broke T2 ammo
Fixed T2 ammo and added tests to avoid same regression.

src/python.worker.js
@@ -157,7 +157,6 @@ def face_to_face_expected_wounds(
         wounds['total_rolls'] += rolls
         if a_crit + a_hit > 0:
             winner = 'active'
-            counterpart = 'reactive'
             armor_save = player_a_dam - player_b_arm
             damage = 2 if player_a_ammo == 'T2' else 1
             cont = player_a_cont
@@ -166,7 +165,6 @@ def face_to_face_expected_wounds(
             bts_save = player_a_dam - player_b_bts
         elif b_crit + b_hit > 0:
             winner = 'reactive'
-            counterpart = 'active'
             armor_save = player_b_dam - player_a_arm
             damage = 2 if player_b_ammo == 'T2' else 1
             cont = player_b_cont
@@ -175,7 +173,6 @@ def face_to_face_expected_wounds(
             bts_save = player_b_dam - player_a_bts
         else:
             winner = 'fail'
-            counterpart = 'missed'
             armor_save = 0
             damage = 0
             cont = False
@@ -197,9 +194,9 @@ def face_to_face_expected_wounds(
         if cont:
             dSave = Die([(damage + Again() if x <= armor_save else 0) for x in range(1, 21)], again_depth=5)
         else:
-            dSave = d20 <= armor_save
-        dCrit = d20 <= armor_save
-        dPlasma = d20 <= bts_save
+            dSave = (d20 <= armor_save) * damage  # T2 ammo increases damage by 1
+        dCrit = d20 <= armor_save  # Crits are always 1 damage
+        dPlasma = d20 <= bts_save  # Plasma BTS hits are always 1 damage (so far)
 
         r = Pool(
             [dSave for x in range(saves)] + [dCrit for x in range(crit_saves)] + [dPlasma for x in range(plasma_saves)]

src/python/f2f.py
@@ -81,7 +81,8 @@ def fixed_face_to_face(player_a_sv, player_a_burst, player_b_sv, player_b_burst)
     a_sv = player_a_sv if player_a_sv <= 20 else 20
     a_bonus = 0 if player_a_sv <= 20 else player_a_sv - 20
     b_sv = 21  # make success value 21 to avoid crits
-    b_die = Die([player_b_sv])  # Create a special die that always rolls the same number
+    b_die_face = player_b_sv if player_b_sv <= 20 else 20  # don't let fixed die be over 20
+    b_die = Die([b_die_face])  # Create a special die that always rolls the same number
 
     result = InfinityFace2FaceEvaluator(a_sv=a_sv, b_sv=b_sv).evaluate(
         lowest(d20+a_bonus, 20).pool(player_a_burst),
@@ -154,7 +155,6 @@ def face_to_face_expected_wounds(
         wounds['total_rolls'] += rolls
         if a_crit + a_hit > 0:
             winner = 'active'
-            counterpart = 'reactive'
             armor_save = player_a_dam - player_b_arm
             damage = 2 if player_a_ammo == 'T2' else 1
             cont = player_a_cont
@@ -163,7 +163,6 @@ def face_to_face_expected_wounds(
             bts_save = player_a_dam - player_b_bts
         elif b_crit + b_hit > 0:
             winner = 'reactive'
-            counterpart = 'active'
             armor_save = player_b_dam - player_a_arm
             damage = 2 if player_b_ammo == 'T2' else 1
             cont = player_b_cont
@@ -172,7 +171,6 @@ def face_to_face_expected_wounds(
             bts_save = player_b_dam - player_a_bts
         else:
             winner = 'fail'
-            counterpart = 'missed'
             armor_save = 0
             damage = 0
             cont = False
@@ -194,9 +192,9 @@ def face_to_face_expected_wounds(
         if cont:
             dSave = Die([(damage + Again() if x <= armor_save else 0) for x in range(1, 21)], again_depth=5)
         else:
-            dSave = d20 <= armor_save
-        dCrit = d20 <= armor_save
-        dPlasma = d20 <= bts_save
+            dSave = (d20 <= armor_save) * damage  # T2 ammo increases damage by 1
+        dCrit = d20 <= armor_save  # Crits are always 1 damage
+        dPlasma = d20 <= bts_save  # Plasma BTS hits are always 1 damage (so far)
 
         r = Pool(
             [dSave for x in range(saves)] + [dCrit for x in range(crit_saves)] + [dPlasma for x in range(plasma_saves)]

src/python/test_f2f.py
@@ -531,6 +531,37 @@ P2 Scores  1+ Successes:  22.705%"""
         assert is_ghostlords_equal(gl_dict, kp_dict)
 
 
+    def test_T2_ammo(self):
+        """This is actually a self test, GL is not calculating T2 correctly at this time"""
+        # TODO: create a testing framework for non ghostlords comparisons
+        print("Testing T2 ammo")
+        active_str = """28.543125% Custom Unit inflicts 1 or more wounds
+27.4625% Custom Unit inflicts 2 or more wounds
+2.006875% Custom Unit inflicts 3 or more wounds"""
+        failure_str = """42.90% Neither player succeeds"""
+        reactive_str = """28.543125% Custom Unit inflicts 1 or more wounds
+2.006875% Custom Unit inflicts 2 or more wounds"""
+        player_a_sv_ = 13
+        player_a_burst_ = 1
+        player_a_dam_ = 13
+        player_a_arm_ = 0
+        player_a_ammo_ = 'T2'
+        player_b_sv_ = 13
+        player_b_burst_ = 1
+        player_b_dam_ = 13
+        player_b_arm_ = 0
+        player_b_ammo_ = 'N'
+        player_a_cont_ = False
+        player_b_cont_ = False
+        outcomes = face_to_face(player_a_sv_, player_a_burst_, player_b_sv_, player_b_burst_)
+        kp_result = face_to_face_expected_wounds(
+            outcomes,
+            player_a_dam_, player_a_arm_, player_a_ammo_,
+            player_b_dam_, player_b_arm_, player_b_ammo_, player_a_cont=player_a_cont_)
+        gl_dict = ghostlords_minimal_to_dict(active_str=active_str, reactive_str=reactive_str, fail_str=failure_str)
+        kp_dict = khepri_result_to_percentage(kp_result, max_wounds_shown=3)
+        assert is_ghostlords_equal(gl_dict, kp_dict)
+
 
 # TODO: Crit immune tests. Note: GL does not have a crit immune toggle
 # TODO: Dodge vs template weapon test